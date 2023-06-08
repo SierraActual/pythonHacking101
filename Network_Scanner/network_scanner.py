935e7edd6c4c33ca20f97026fd5e9ffce3406d10
Chris <chrispitillo@gmail.com>
2023-06-08 18:55:32 +0100

    network scanner progress

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,2 +1,14 @@
 import scapy.all as scapy
 
+
+def main():
+    scan("209.151.148.1")
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+
+
+if __name__ == "__main__":
+    main()
