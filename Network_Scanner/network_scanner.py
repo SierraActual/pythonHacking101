d2ed1e1bb17484c563ec7140b6f0c1a417a1b0c6
SierraActual <chrispitillo@gmail.com>
2023-06-10 07:27:31 -0400

    Fixed network scanner

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -3,26 +3,26 @@ import argparse
 
 
 def main():
-    target = get_arguments
+    target = get_arguments()
     check_args(target)
     #perform a scan of clients on the specified network
     found_connections = scan(target)
 
     #print results returned from scan
-    print('IP\t\t\tMAC Address\n---------------------------------------------------')
+    print('ip\t\t\tmac Address\n---------------------------------------------------')
     for client in found_connections:
-        print(f'{client['IP']}\t\t{client['MAC']}')
+        print(f'{client["ip"]}\t\t{client["mac"]}')
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', dest='target', help="IP that user wishes to scan.")
+    parser.add_argument('-t', '--target', dest='target', help="ip that user wishes to scan.")
     options = parser.parse_args()
 
     return options.target
 
 
-def check_args(interface, new_mac):
+def check_args(target):
     if target == None:
         print('Please input a target to scan using the "-t" command.\n(Use --help for help)')
         exit()
@@ -41,11 +41,11 @@ def scan(ip):
     #creates a list to return values
     found_connections = []
 
-    #appends our found_connections list with dicts of parsed IP/MAC info
+    #appends our found_connections list with dicts of parsed ip/mac info
     for element in answered_list:
         clientDict = {
-            'IP': element[1].psrc,
-            'MAC': element[1].hwsrc
+            'ip': element[1].psrc,
+            'mac': element[1].hwsrc
             }
         found_connections.append(clientDict)
     
