cd52434c21cee280a6e4e1b86f994695fe0bcf0f
Chris <chrispitillo@gmail.com>
2023-06-23 17:05:14 -0400

    Automated iptables commands, but needs work to ensure flush actually runs on ^c

--- cut_net/cut_net.py
+++ cut_net/cut_net.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 import netfilterqueue
+import pyptables
 
 def main():
-    #Need to create network que using iptables command:
-        #iptables -I FORWARD -j NFQUEUE --queue-num 0
-    #Upon completion execute flush command:
-        #iptables --flush
-    #TODO automate above.
-    
-    queue = netfilterqueue.NetfilterQueue()
-    queue.bind(0, process_packet)
-    queue.run()
+    setup_iptables()
+
+    try:
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0, process_packet)
+        queue.run()
+    except KeyboardInterrupt:
+        #TODO This may cause issues because except code isn't executing properly in my enviornment
+        print('Resetting IP tables and exiting...')
+        flush_iptables()
+
+
+def setup_iptables():
+    table = pyptables.Table('filter')
+    chain = table.chain('FORWARD')
+    rule = pyptables.Rule()
+    target = pyptables.Target('NFQUEUE')
+    target.set_parameter('queue-num', '0')
+    rule.target = target
+    chain.insert_rule(rule, position=0)
+
+
+def flush_iptables():
+    table = pyptables.Table('filter')
+    chain = table.chain('FORWARD')
+    chain.flush()
 
 
 def process_packet(packet):
