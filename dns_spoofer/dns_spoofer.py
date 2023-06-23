0c22f0b87934049b5a78966a98bc0a935a9039a7
Chris <chrispitillo@gmail.com>
2023-06-23 17:19:42 -0400

    Initial dns_spoofer program

---
+++ dns_spoofer/dns_spoofer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import netfilterqueue
+import pyptables
+import scapy
+
+def main():
+    setup_iptables()
+
+    try:
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0, process_packet)
+        queue.run()
+    except KeyboardInterrupt:
+        #TODO This may cause issues because except code isn't executing properly in my enviornment
+        print('Resetting IP tables and exiting...')
+        flush_iptables()
+
+
+def setup_iptables():
+    table = pyptables.Table('filter')
+    chain = table.chain('FORWARD')
+    rule = pyptables.Rule()
+    target = pyptables.Target('NFQUEUE')
+    target.set_parameter('queue-num', '0')
+    rule.target = target
+    chain.insert_rule(rule, position=0)
+
+
+def flush_iptables():
+    table = pyptables.Table('filter')
+    chain = table.chain('FORWARD')
+    chain.flush()
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
+    packet.accept()
+
+
+if __name__ == '__main__':
+    main()
