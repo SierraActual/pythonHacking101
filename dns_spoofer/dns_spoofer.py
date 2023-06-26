7604bbd7c7f9b11867b9f4d5deeb48310f3af9fc
chris <chrispitillo@gmail.com>
2023-06-26 16:45:54 +0100

    Minor change to stop iptables automation for now

--- dns_spoofer/dns_spoofer.py
+++ dns_spoofer/dns_spoofer.py
@@ -12,7 +12,7 @@ def main():
     except KeyboardInterrupt:
         #TODO This may cause issues because except code isn't executing properly in my enviornment
         print('Resetting IP tables and exiting...')
-        flush_iptables()
+        #flush_iptables()
 
 
 def setup_iptables():
