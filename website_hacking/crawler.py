ba1f8104eebf537b9170086d877fd86d159b5430
Chris <chrispitillo@gmail.com>
2023-07-14 19:09:55 -0400

    Crawler work

--- website_hacking/crawler.py
+++ website_hacking/crawler.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
-import requests
+import requests, re
+from urllib.parse import urljoin
+
+#TODO add function to get rid of appends to list if limitless domains are detected
+#TODO need to get rid of limitless function and replace with a redirect finder
+    # redirect should skip that domain in the list and move to the next domain without appending to target_links
 
 def main():
     crawler = Crawler('google.com')
@@ -8,18 +13,23 @@ def main():
 class Crawler:
     def __init__(self, url):
         self.url = url
+        self.target_links = set()
 
     def start(self):
+        self.full_search()
+        print(f'[+] Search Complete.\n[+] Full site listing as follows:\n' + '\n'.join(self.target_links))
+
+    def full_search(self):
         try:
             print('[+] Starting directory search with default URL...')
-            self.dir_search(f'http://{self.url}')
+            self.dir_search(f'https://{self.url}')
             
             print('[+] Starting subdomain search...')
-            with open('sample_wordlist.txt', 'r') as wordlist_file:
+            with open('short_sample_wordlist.txt', 'r') as wordlist_file:
+                previous_string = ''
                 for line in wordlist_file:
-                    previous_string = ''
                     word = line.strip()
-                    full_url = f'{word}.{self.url}'
+                    full_url = f'https://{word}.{self.url}'
 
                     # Dynamic updating:
                     print(f'Current search: {full_url.ljust(len(previous_string))}\r', end='')
@@ -28,6 +38,8 @@ class Crawler:
                     response = self.request(full_url)
                     if response:
                         print(f'[+] {full_url} status: {response}\n    [+] Searching for additional directories...')
+                        self.target_links.add(full_url)
+                        self.spider(full_url)
                         self.dir_search(full_url)
         except KeyboardInterrupt:
             print('\n[+] Interrupt detected. Exiting...')
@@ -35,22 +47,15 @@ class Crawler:
 
     def dir_search(self, url, visited_domains=set()):
         try:
-            domain = url.split('/')[2]  # Extract the domain from the URL
-            if domain in visited_domains:
-                print(f'   [!] Infinite recursion detected at: {domain}')
+            if self.request(url):
+                print(f'      [!] Potential limitless domains at: {url}.')
                 return
-
-            visited_domains.add(domain)
         except Exception:
             pass
 
         with open('short_sample_dirlist.txt', 'r') as dir_list:
             previous_string = ''
-            counter = 0
             for line in dir_list:
-                if counter > 10:
-                    print(f'      [!] Potential limitless domains at: {url}.')
-                    return
                 word = line.strip()
                 full_url = f'{url}/{word}'
 
@@ -61,6 +66,8 @@ class Crawler:
                 response = self.request(full_url)
                 if response:
                     print(f'    [+] Directory found: {full_url}')
+                    self.spider(full_url)
+                    self.target_links.add(full_url)
                     self.dir_search(full_url, visited_domains)
                     counter += 1
         try:
@@ -70,9 +77,21 @@ class Crawler:
 
     def request(self, url):
         try:
-            return requests.get(f'http://{url}', timeout=2)
+            return requests.get(f'{url}', timeout=2)
         except Exception:
             pass
+
+    def spider(self, url):
+        response = requests.get(url)
+        href_links = re.findall(r'(?:href=")(.*?):"', response.content.decode())
+        for link in href_links:
+            link = urljoin(url, link)
+            if url in link:
+                self.target_links.add(link)
+
+    def check_infinite(self, url):
+        url = f'{url}/sdauhdfoiasuhfioasuhdfiushdfkjaekjfoiaewuhfbfaiosudbfhoiusadhfoiueahfiealsnbfoiasuefhiuoaseygf'
+        self.request(url)
 
 if __name__ == '__main__':
     main()
